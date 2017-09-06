dfe451307df873ac68d4366f11c1227885f876e3
Amanda <mandakom@uw.edu>
2017-09-05 20:04:36 -0700

    Arrays and Loops examples

--- helloWorld.js
+++ helloWorld.js
@@ -1,4 +1,40 @@
-document.write('Hey Brussels Sprout, you rock!');
+var animals = [
+  'rabbit',
+  'frog',
+  'dog',
+  'giraffe',
+  'bear',
+  'zebra',
+  'horse',
+  'squirrel',
+  'bird',
+  'alligator',
+  'crocodile'
+];
+
+var currentAnimal = 0;
+
+var numbers = [1, 3, 5, 7, 9, 10];
+var sum = 0;
+var currentNumber = 0;
+
 window.onload = function() {
-  document.getElementById('greeting').append(' What should we learn today?');
+  do {
+    document.getElementById('message').append(animals[currentAnimal]);
+    document.getElementById('message').append("\n");
+    currentAnimal += 1;
+  } while (currentAnimal < animals.length);
+
+  document.getElementById('message').append('ALL DONE!!');
+
+  do {
+    sum += numbers[currentNumber];
+    document.getElementById('message').append(
+      sum
+    );
+    document.getElementById('message').append("\n");
+    currentNumber += 1;
+  } while (currentNumber < numbers.length);
+
+  document.getElementById('message').append('ALL DONE!!');
 };
